e772b766b458ccd9f1965e5839c76469a503c24e
user <apps.tacko@gmail.com>
2025-03-30 22:18:27 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Fonts from './fonts.json'
+import ConvertList from './letter_convert_list.json'
 import './App.css';
 
-function App() {
+/**
+ * 読み込むフォントリスト
+ */
+const fontList = Fonts;
+
+/**
+ * 絵文字名変換表
+ */
+const convertList: {[key: string]: string} = ConvertList;
+
+const Main = () => {
+  /** 絵文字にする文字列 */
+  const [text, setText] = useState('');
+
+  /** フォント名 */
+  const [fontName, setFontName] = useState(fontList[0] !== undefined ? fontList[0].name : '');
+
+  const createEmojiName = () => {
+    Object.keys(convertList).forEach((letter: string) => {
+      text.replaceAll(letter, convertList[letter]);
+    }); 
+  }
+  /**
+   * テキストエリア要素変更イベント
+   * @param e textarea要素変更イベント
+   */
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div>
+        {/* テキスト */}
+        <textarea value={text} style={{fontFamily: fontName}} onChange={handleTextChange} />
+      </div>
+      <div>
+        <span className='font'>
+          {
+            /* フォント名 */
+            Fonts.map((font) => {
+              return <button name={font.name} value={font.name} style={{fontFamily: font.name}} onClick={() => { setFontName(font.name) }}>{font.displayName}</button>
+            })
+          }
+        </span>
+      </div>
     </div>
+  );
+}
+
+const App = () => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href={`https://fonts.googleapis.com/css2?${Fonts.map((font) => 'family=' + font.name.replaceAll(' ', '+')).join('&')}&display=swap`} rel="stylesheet" />
+      <div className="main">
+        <Main />
+      </div>
+    </>
   );
 }
 
